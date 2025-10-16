a61da590b6ecb828e69700cf9090bd73479a30eb
Nico Ferrante <nicola.ferrante@researchspace.com>
2025-10-16 14:39:40 +0100

    RSDEV-848: java Docs

--- src/main/java/com/researchspace/raid/client/RaIDClient.java
+++ src/main/java/com/researchspace/raid/client/RaIDClient.java
@@ -9,23 +9,52 @@ import org.springframework.web.client.HttpServerErrorException;
 public interface RaIDClient {
 
 
-  //  GET /service-point/: returns all the service points associated to the current user
+  /***
+   * This method is in charge to call the RaID end point to receive the list of the Service Points
+   * that the user {@param accessToken} can see
+   *
+   * @param accessToken the api token associated to the user
+   * @return a Set of Service Point
+   * @throws HttpServerErrorException
+   */
   Set<RaIDServicePoint> getServicePointList(String accessToken)
       throws HttpServerErrorException;
 
 
-  //  GET /service-point/{id}: returns the metadata of the service point
+  /***
+   * This method is in charge to call the RaID end point to receive
+   * the data and metadata of a given Service Point {@param servicePointId}
+   *
+   * @param accessToken the api token associated to the user
+   * @param servicePointId the (numeric) ID of the service point
+   * @return Data and metadata associated to the service point
+   * @throws HttpServerErrorException
+   */
   RaIDServicePoint getServicePoint(String accessToken, Integer servicePointId)
       throws HttpServerErrorException;
 
 
-  //  GET /raid/: return the list of the RaID associated with the user (availability to filer by contributorId and/or organisationId)
-  Set<RaID> getRaidList(String accessToken)
-      throws HttpServerErrorException;
+  /***
+   * This method is in charge to call the RaID end point to receive
+   * the Set of RaID associated to the user {@param accessToken}
+   *
+   * @param accessToken the api token associated to the user
+   * @return the Set of RaID associated to the current user
+   * @throws HttpServerErrorException
+   */
+  Set<RaID> getRaidList(String accessToken) throws HttpServerErrorException;
 
 
-  //  GET /raid/{prefix}/{suffix}: returns all the data and metadata associated to a given RaID
-  RaID getRaid(String accessToken, String raidPrefix, String raidSuffix)
-      throws HttpServerErrorException;
+  /***
+   * This method is in charge to call the RaID end point to receive data and
+   * metadata of a given RaID defined by {@param raidPrefix} and {@param raidSuffix}
+   *
+   * @param accessToken the api token associated to the user
+   * @param raidPrefix The RaID prefix (i.e.: "10.33334")
+   * @param raidSuffix The RaID suffix (i.e.: "c74980b1")
+   * @return data and metadata of a given RaID
+   * @throws HttpServerErrorException
+   */
+  RaID getRaid(String accessToken, String raidPrefix, String raidSuffix) throws HttpServerErrorException;
 
 }
