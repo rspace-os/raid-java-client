c71292b1dde6c9966e5ab07a4c7169c44a7a1ca1
Nico Ferrante <nicola.ferrante@researchspace.com>
2026-01-08 12:08:06 +0000

    RSDEV-945: Apply Fraser's code review comments (part 2)

--- src/main/java/com/researchspace/raid/model/RaIDRelatedObject.java
+++ src/main/java/com/researchspace/raid/model/RaIDRelatedObject.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @ToString(callSuper = true)
 public class RaIDRelatedObject extends RaIDBriefIdentifier {
 
-  private static final String DOI_RELATED_OBJECT_SCHEMA = "https://doi.org/";
+  private static final String DOI_SCHEMA_URL = "https://doi.org/";
   private static final String EVENT_TYPE_ID = "https://vocabulary.raid.org/relatedObject.type.schema/260";
   private static final String EVENT_TYPE_SCHEMA = "https://vocabulary.raid.org/relatedObject.type.schema/329";
   private static final String OUTPUT_CATEGORY_ID = "https://vocabulary.raid.org/relatedObject.category.id/190";
@@ -25,7 +25,7 @@ public class RaIDRelatedObject extends RaIDBriefIdentifier {
 
 
   public RaIDRelatedObject(String identifier){
-    super(identifier, DOI_RELATED_OBJECT_SCHEMA);
+    super(identifier, DOI_SCHEMA_URL);
     this.type = new RaIDType(EVENT_TYPE_ID, EVENT_TYPE_SCHEMA);
     this.category = List.of(new RaIDCategory(OUTPUT_CATEGORY_ID, OUTPUT_CATEGORY_SCHEMA));
   }
